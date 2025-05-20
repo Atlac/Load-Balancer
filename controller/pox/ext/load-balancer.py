701d5fcf39377439f7086de527fceb0d56a30801
Federico Caltabiano <caltabiano.2213914@studenti.uniroma1.it>
2025-05-20 20:59:10 +0200

    Changes in load-balancer.py file

--- controller/pox/ext/load-balancer.py
+++ controller/pox/ext/load-balancer.py
@@ -1,21 +1,20 @@
 from pox.core import core
 from pox.lib.packet.ethernet import ethernet
 from pox.lib.packet.ipv4 import ipv4
-from pox.lib.packet.arp import arp
-from pox.lib.addresses import IPAddr, EthAddr
-from pox.lib.util import dpid_to_str
+from pox.lib.addresses import IPAddr
 from pox.openflow import libopenflow_01 as of
+from routing_algorithm import get_next_hop  # Import the external algorithm
 
 log = core.getLogger()
 
-# Define the source and destination IPs
-SOURCE_IP = "10.0.0.1"
-DESTINATION_IP = "30.0.0.1"
+class LoadBalancer(object):
+    def __init__(self):
+        log.info("Initializing Load Balancer")
+        core.openflow.addListeners(self)
 
-class loadbalancer(object):
-    def _handle_PacketIn(event):
+    def _handle_PacketIn(self, event):
         """
-        Handles incoming packets and forwards packets from SOURCE_IP to DESTINATION_IP.
+        Handles incoming packets and routes them based on the external algorithm.
         """
         packet = event.parsed
 
@@ -27,36 +26,41 @@ class loadbalancer(object):
         if packet.type == ethernet.IP_TYPE:
             ip_packet = packet.payload
 
-            if isinstance(ip_packet, ipv4) and ip_packet.srcip == IPAddr(SOURCE_IP):
-                log.info(f"Packet received from {SOURCE_IP}, redirecting to {DESTINATION_IP}")
+            if isinstance(ip_packet, ipv4):
+                log.info(f"Packet received from {ip_packet.srcip} to {ip_packet.dstip}")
 
-                # Modify the destination IP
-                ip_packet.dstip = IPAddr(DESTINATION_IP)
+                # Use the external algorithm to determine the next hop
+                next_hop = get_next_hop(str(ip_packet.srcip), str(ip_packet.dstip))
 
-                # Create a new packet to send
-                new_packet = ethernet()
-                new_packet.src = packet.src
-                new_packet.dst = packet.dst
-                new_packet.type = packet.type
-                new_packet.payload = ip_packet
+                if next_hop:
+                    log.info(f"Routing packet to {next_hop}")
 
-                # Send the modified packet out
-                msg = of.ofp_packet_out()
-                msg.data = new_packet.pack()
-                msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
-                event.connection.send(msg)
-                return
+                    # Modify the destination IP
+                    ip_packet.dstip = IPAddr(next_hop)
+
+                    # Create a new packet to send
+                    new_packet = ethernet()
+                    new_packet.src = packet.src
+                    new_packet.dst = packet.dst
+                    new_packet.type = packet.type
+                    new_packet.payload = ip_packet
+
+                    # Send the modified packet out
+                    msg = of.ofp_packet_out()
+                    msg.data = new_packet.pack()
+                    msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+                    event.connection.send(msg)
+                    return
 
         # If the packet doesn't match, flood it
         msg = of.ofp_packet_out()
         msg.data = event.ofp
         msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
         event.connection.send(msg)
-        
 
 def launch():
     """
     Launches the POX controller.
     """
     log.info("Starting POX Load Balancer Controller")
-    core.openflow.addListeners(loadbalancer._handle_PacketIn)
+    LoadBalancer()
